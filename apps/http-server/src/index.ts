98a6acba146c05a911caf818a7b8dee70713a866
Ashish Pal <ashish81301669@gmail.com>
2025-03-11 12:42:14 +0530

    first commit

---
+++ apps/http-server/src/index.ts
@@ -0,0 +1,27 @@
+import express from "express";
+import { client } from "@repo/db/client";
+
+const app = express();
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Hi there");
+});
+
+app.post("/signup", async (req, res) => {
+  const { username, password } = req.body;
+
+  const user = await client.user.create({
+    data: {
+      username,
+      password,
+    },
+  });
+
+  res.status(200).json({
+    userId: user.id,
+    message: "signup Successfull",
+  });
+});
+
+app.listen(3000);
